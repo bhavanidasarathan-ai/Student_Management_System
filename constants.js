f34c6166d42b106d46f43a37ace1e128b648565c
dhanyapandiyaraja-cpu <dhanyapandiyaraja@gmail.com>
2026-07-27 22:47:32 +0530

    constants.js

---
+++ constants.js
@@ -0,0 +1,21 @@
+export const DEPARTMENTS = [
+  "Computer Science",
+  "Software Systems",
+  "Information Technology",
+  "Artificial Intelligence",
+];
+
+export const YEARS = [
+  "I Year",
+  "II Year",
+  "III Year",
+];
+
+export const SEMESTERS = [
+  "Semester 1",
+  "Semester 2",
+  "Semester 3",
+  "Semester 4",
+  "Semester 5",
+  "Semester 6",
+];
