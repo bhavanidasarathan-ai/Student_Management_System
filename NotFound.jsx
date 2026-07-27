39f872c89c01522859ef7975f156fbfb978272cc
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-27 22:02:11 +0530

    NotFound.jsx

---
+++ NotFound.jsx
@@ -0,0 +1,17 @@
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+      }}
+    >
+      404 - Page Not Found
+    </div>
+  );
+};
+
+export default NotFound;
