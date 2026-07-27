a2f0e8aaaacebabce6b3bd16ef1a52a4aefc1081
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-27 22:50:51 +0530

    Loader.jsx

---
+++ Loader.jsx
@@ -0,0 +1,15 @@
+const Loader = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        fontSize: "20px",
+        padding: "30px",
+      }}
+    >
+      Loading...
+    </div>
+  );
+};
+
+export default Loader;
