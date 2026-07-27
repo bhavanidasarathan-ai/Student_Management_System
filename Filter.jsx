7e6e33a61f7fa6a9033f284b25a7f1e8c4fa8939
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-27 22:50:16 +0530

    Filter.jsx

---
+++ Filter.jsx
@@ -0,0 +1,19 @@
+const Filter = () => {
+  return (
+    <select
+      style={{
+        padding: "10px",
+        marginLeft: "20px",
+        borderRadius: "8px",
+      }}
+    >
+      <option>All Departments</option>
+      <option>Software Systems</option>
+      <option>Computer Science</option>
+      <option>AI & DS</option>
+      <option>IT</option>
+    </select>
+  );
+};
+
+export default Filter;
