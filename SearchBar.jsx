cdab3247d37a6503d6102a7d3c445b09bab6a6c8
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-27 22:48:24 +0530

    SearchBar.jsx

---
+++ SearchBar.jsx
@@ -0,0 +1,17 @@
+const SearchBar = () => {
+  return (
+    <input
+      type="text"
+      placeholder="🔍 Search Student..."
+      style={{
+        width: "300px",
+        padding: "10px",
+        borderRadius: "8px",
+        border: "1px solid #ccc",
+        marginBottom: "20px",
+      }}
+    />
+  );
+};
+
+export default SearchBar;
