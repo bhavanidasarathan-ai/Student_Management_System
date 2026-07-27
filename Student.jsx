d209c4fd6a72cba85f54ea973e422cbdb2909c38
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-27 22:46:04 +0530

    Student.jsx

--- Student.jsx
+++ Student.jsx
@@ -1,3 +1,5 @@
+import DashboardLayout from "../layouts/DashboardLayout";
+
 const students = [
   {
     id: 1,
@@ -8,14 +10,14 @@ const students = [
   },
   {
     id: 2,
-    name: "Rahul",
+    name: "Dhanya",
     department: "Computer Science",
     year: "III",
     attendance: "90%",
   },
   {
     id: 3,
-    name: "Anjali",
+    name: "Bhavani",
     department: "AI & DS",
     year: "II",
     attendance: "98%",
@@ -24,60 +26,62 @@ const students = [
 
 const Students = () => {
   return (
-    <div style={{ padding: "30px" }}>
-      <h1>Student Management</h1>
+    <DashboardLayout>
+      <div style={{ padding: "30px" }}>
+        <h1>Student Management</h1>
 
-      <button
-        style={{
-          padding: "10px 20px",
-          background: "#2563eb",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-          margin: "20px 0",
-          cursor: "pointer",
-        }}
-      >
-        + Add Student
-      </button>
+        <button
+          style={{
+            padding: "10px 20px",
+            background: "#2563eb",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            margin: "20px 0",
+            cursor: "pointer",
+          }}
+        >
+          + Add Student
+        </button>
 
-      <table
-        border="1"
-        cellPadding="10"
-        style={{
-          width: "100%",
-          borderCollapse: "collapse",
-          textAlign: "center",
-        }}
-      >
-        <thead style={{ background: "#2563eb", color: "white" }}>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Department</th>
-            <th>Year</th>
-            <th>Attendance</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {students.map((student) => (
-            <tr key={student.id}>
-              <td>{student.id}</td>
-              <td>{student.name}</td>
-              <td>{student.department}</td>
-              <td>{student.year}</td>
-              <td>{student.attendance}</td>
-              <td>
-                <button>Edit</button>{" "}
-                <button>Delete</button>
-              </td>
+        <table
+          border="1"
+          cellPadding="10"
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+            textAlign: "center",
+          }}
+        >
+          <thead style={{ background: "#2563eb", color: "white" }}>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Department</th>
+              <th>Year</th>
+              <th>Attendance</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+
+          <tbody>
+            {students.map((student) => (
+              <tr key={student.id}>
+                <td>{student.id}</td>
+                <td>{student.name}</td>
+                <td>{student.department}</td>
+                <td>{student.year}</td>
+                <td>{student.attendance}</td>
+                <td>
+                  <button>Edit</button>{" "}
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </DashboardLayout>
   );
 };
 
