801af347d6e031eabe98cf3efcb350ba04ab85c9
priyavallim11-ai <priyavallim11@gmail.com>
2026-07-27 22:41:19 +0530

    Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        marginTop: "40px",
+        textAlign: "center",
+        color: "#64748b",
+        padding: "20px",
+      }}
+    >
+      © 2026 Smart Student Management System
+    </footer>
+  );
+};
+
+export default Footer;
