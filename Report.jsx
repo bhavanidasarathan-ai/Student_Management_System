c3230233541226e9382c3be67846e62bb3f65548
dhanyapandiyaraja-cpu <dhanyapandiyaraja@gmail.com>
2026-07-27 23:28:13 +0530

    Report.jsx

---
+++ Report.jsx
@@ -0,0 +1,27 @@
+function Report(){
+
+window.print();
+
+return(
+
+<div>
+
+<h1>
+
+Student Management Report
+
+</h1>
+
+<p>
+
+Print Preview
+
+</p>
+
+</div>
+
+)
+
+}
+
+export default Report;
